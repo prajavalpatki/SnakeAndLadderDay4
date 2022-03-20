5109d65436774bdaf089c12ad7d2f3780f2d1a7a
prajavalpatki <prajavalpatki.com>
2022-03-20 17:39:29 +0530

    get a number between 0 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,28 @@
 package com.Bridgelabz;
 
-public class SnakeAndLadder {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
+public class SnakeAndLadder {
+    static Random random = new Random();
+    static int getRandomDieValue() {
+        int randomValue = random.nextInt(6) + 1;
+        return randomValue;
+    }
+    public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder Problem");
 
+        Scanner in = new Scanner(System.in);
         int startPosition = 0;
         System.out.println("Game starts from : " + startPosition);
+        System.out.println("Enter 1 to roll a die");
+        int die = in.nextInt();
+        if(die == 1){
+            int playerDie = getRandomDieValue();
+            System.out.println("Die Face: " +playerDie);
+        }
+        else {
+            System.exit(0);
+        }
     }
 }
