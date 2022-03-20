68385456b0431eaf597175a08f8db5fb0184c400
prajavalpatki <prajavalpatki.com>
2022-03-20 17:23:22 +0530

    Start positoin zero

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.Bridgelabz;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Snake and Ladder Problem");
+
+        int startPosition = 0;
+        System.out.println("Game starts from : " + startPosition);
+    }
+}
