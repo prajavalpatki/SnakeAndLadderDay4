4907c9234928f88234b87bba96745446c244cb2b
prajavalpatki <prajavalpatki.com>
2022-03-20 17:50:10 +0530

    Ladder or Snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,5 +1,4 @@
 package com.Bridgelabz;
-
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,17 +8,38 @@ public class SnakeAndLadder {
         int randomValue = random.nextInt(6) + 1;
         return randomValue;
     }
+
+    static int getRandomOption() {
+        int randomValue1 = random.nextInt(3);
+        return randomValue1;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder Problem");
 
         Scanner in = new Scanner(System.in);
         int startPosition = 0;
+        int currentPosition = 0;
         System.out.println("Game starts from : " + startPosition);
         System.out.println("Enter 1 to roll a die");
         int die = in.nextInt();
-        if(die == 1){
+        if (die == 1) {
             int playerDie = getRandomDieValue();
-            System.out.println("Die Face: " +playerDie);
+            System.out.println("Die Face: " + playerDie);
+            int option = getRandomOption();
+            switch (option) {
+                case 1:
+                    System.out.println("Option: LADDER");
+                    currentPosition += playerDie;
+                    break;
+                case 2:
+                    System.out.println("Option: SNAKE");
+                    currentPosition -= playerDie;
+                    break;
+                default:
+                    System.out.println("Option: NO PLAY");
+                    break;
+            }
+            System.out.println("Current Position: " + currentPosition);
         }
         else {
             System.exit(0);
